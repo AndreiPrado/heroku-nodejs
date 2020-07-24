4f379146b54229f6a50c794c1b4de40dc938005c
Andrei Prado <andrei.prado@hotmail.com>
2020-07-23 23:02:53 -0300

    Corrige angular.json

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express()
 
-
 app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
+
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
